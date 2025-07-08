75154784b033d01d506453b61e63ede19eb5d8cf
ZaynSol <zaynsolomonmar@gmail.com>
2025-07-08 18:10:44 +0300

    Fix 6-multi_languages_loop.js: use join(), remove if and use one console.log

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,11 +1,2 @@
 const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-let output = "";
-
-for (let i = 0; i < lines.length; i++) {
-  output += lines[i];
-  if (i < lines.length - 1) {
-    output += "\n";
-  }
-}
-
-console.log(output);
+console.log(lines.join("\n"));
